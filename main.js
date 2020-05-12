b45afb8f4e56e4bf2c05fc6684bc558f566ad74a
Dorota Pindur <dpindur@wp.pl>
2020-05-12 20:09:27 +0200

    playing with simple JS functions

--- main.js
+++ main.js
@@ -7,4 +7,44 @@ console.log('Na razie piszę sobie coś w ramach zadania domowego.');
 console.log('Chciałabym, żeby program rozróżniał przy podawaniu wieku "lat"/"lata", ale jeszcze nie wiem, jak to zrobić');
 
 const appearingParagraph = document.querySelector('.inner-section__text--js');
-appearingParagraph.innerHTML = `W tym miejscu pojawia się paragraf wstawiony przez JS - jest widoczny w DOM, ale nie w kodzie źródłowym`;
+appearingParagraph.innerHTML = `W tym miejscu pojawia się paragraf wstawiony przez JS - jest widoczny w DOM, ale nie w kodzie źródłowym.`;
+
+function calculate(myNumber) {
+    console.log(`Podana liczba to ${myNumber}`)
+    return myNumber*7
+}
+
+const myResult = calculate(age);
+console.log(`Wynik mnożenia *7 to ${myResult}`);
+
+
+let myNumber = "3";
+const timesFive = (myNumber) => {
+    console.log(`Podana liczba to ${myNumber}`)
+    return myNumber*5;
+}
+let timesFiveResult = timesFive(myNumber);
+console.log(`Wynik mnożenia liczby ${myNumber} razy 5 wynosi ${timesFiveResult}`);
+
+
+myNumber = "5";
+timesFiveResult = timesFive(myNumber);
+console.log(`Wynik mnożenia liczby ${myNumber} razy 5 wynosi ${timesFiveResult}`);
+
+
+function greet(age, myName) {
+    console.log(`Nazywam się ${myName}, mam ${age} lat(a) i sprawdzam właśnie, czy moja funkcja pokaże mi ten tekst`);
+}
+greet(74, 'Franio');
+greet(36, "Dora");
+
+
+const weekNumber = '5';
+
+function createContent(querySelectorContent, content) {
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+}
+createContent('.inner-section__another-text--js', `A to jest tekst wstawiony za pomocą funkcji o nazwie createContent. W zasadzie ten tekst powinien być w ${weekNumber} tygodniu. Numer tygodnia jest określony za pomocą zmiennej const weekNumber = '5'.`);
+createContent('.section__welcome-heading--js', `Cześć! Witam na moim poligonie eksperymentalnym.`);
+
